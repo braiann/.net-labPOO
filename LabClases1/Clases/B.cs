ae3db5d79b9eb3cd24ab3cbb5d0e8d743bda95d0
Braian <braian.villsanti2@gmail.com>
2020-04-26 17:19:31 -0300

    first commit

---
+++ LabClases1/Clases/B.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    public class B : A
+    {
+        public B() : base("Instancia de B") {}
+
+        public void M4()
+        {
+            Console.WriteLine("Método del hijo invocado.");
+        }
+    }
+}
