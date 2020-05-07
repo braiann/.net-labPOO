4d8c22d1e7640c577b089c15e3fd50abc7854dc5
Braian <braian.villsanti2@gmail.com>
2020-05-07 12:38:44 -0300

    Adivine el número

---
+++ AdivineElNumero/Juego.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivineElNumero
+{
+    class Juego
+    {
+        public void ComenzarJuego()
+        {
+            bool seguirJugando = true;
+            int record = -1;
+
+            while (seguirJugando)
+            {
+                Console.WriteLine("JUEGO INICIADO");
+                Jugada j = new Jugada(PreguntarMaximo());
+
+                while (!j.Adivino)
+                {
+                    PreguntarNumero(j);
+                }
+                Console.WriteLine("Adivinaste en " + j.Intentos + " intentos.");
+
+                if (record == -1)
+                {
+                    record = j.Intentos;
+                }
+                else if (CompararRecord(j.Intentos, record))
+                {
+                    record = j.Intentos;
+                    Console.WriteLine("Nuevo récord! " + record + " intentos.");
+                }
+
+                seguirJugando = Continuar();
+                Console.Clear();
+            }
+        }
+
+        private int PreguntarMaximo()
+        {
+            Console.Write("Ingrese el número máximo: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        // Devuelve true si el recordA es menor al recordB
+        private bool CompararRecord(int recordA, int recordB)
+        {
+            return recordA < recordB;
+        }
+
+        private bool Continuar()
+        {
+            Console.WriteLine("Presione 'C' para continuar jugando...");
+            return Console.ReadKey(true).Key.Equals(ConsoleKey.C) ? true : false;
+        }
+
+        private void PreguntarNumero(Jugada jugada)
+        {
+            int numeroIngresado;
+            Console.Write("Adivina el número: ");
+            numeroIngresado = int.Parse(Console.ReadLine());
+            jugada.Comparar(numeroIngresado);
+        }
+    }
+}
---
+++ AdivineElNumero/Jugada.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivineElNumero
+{
+    class Jugada
+    {
+        private int numero;
+        private bool adivino;
+        private int intentos;
+
+        public int Numero
+        {
+            get => numero;
+            set => numero = value;
+        }
+        public int Intentos
+        {
+            get => intentos;
+            set => intentos = value;
+        }
+        public bool Adivino
+        {
+            get => adivino;
+            set => adivino = value;
+        }
+
+        public bool Comparar(int n)
+        {
+            Adivino = Numero == n;
+            Intentos++;
+            return Adivino;
+        }
+        
+        public Jugada(int maxNumero)
+        {
+            Random rnd = new Random();
+            Numero = rnd.Next(maxNumero);
+        }   
+    }
+}
